69e63be9160665c0cfed3933dddd86545712549a
binaryBard97 <snp8100@gmail.com>
2024-07-21 15:10:47 -0400

    Add lab5

--- lab4/NBClassifier.java
+++ lab4/NBClassifier.java
@@ -1,129 +1,40 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class NBClassifier {
-   String[] trainingDocs;
-   int[] trainingLabels;
-   int numClasses;
-   int[] classCounts; // number of docs per class
-   String[] classStrings; // concatenated string for all terms in a class
-   int[] classTokenCounts; // total number of terms per class (includes duplicate terms) or T_ct
-   HashMap<String, Double>[] condProb; // one hash map for each class
-   HashSet<String> vocabulary; // entire vocabuary
+    String[] trainingDocs;
+    int[] trainingLabels;
+    int numClasses;
+    int[] classCounts; // number of docs per class
+    String[] classStrings; // concatenated string for all terms in a class
+    int[] classTokenCounts; // total number of terms per class (includes duplicate terms) or T_ct
+    HashMap<String, Double>[] condProb; // one hash map for each class
+    HashSet<String> vocabulary; // entire vocabuary
 
-   @SuppressWarnings("unchecked")
-   public NBClassifier(String[] docs, int[] labels, int numC) {
-      trainingDocs = docs;
-      trainingLabels = labels;
-      numClasses = numC;
-      classCounts = new int[numClasses];
-      classStrings = new String[numClasses];
-      classTokenCounts = new int[numClasses];
-      condProb = new HashMap[numClasses];
-      vocabulary = new HashSet<String>();
+    public NBClassifier(String trainDataFolder) throws IOException {
 
-      for (int i = 0; i < numClasses; i++) {
-         classStrings[i] = "";
-         condProb[i] = new HashMap<String, Double>();
-      }
+    }
 
-      for (int i = 0; i < trainingLabels.length; i++) {
-         // classCounts = [0, 0] and classStrings = ["", ""]
-         classCounts[trainingLabels[i]]++;
-         classStrings[trainingLabels[i]] += (trainingDocs[i] + " "); // add the document content to the class string
+    public int classify(String testDoc) {
+        int label = 0;
+        return label;
+    }
 
-         // After processing
-         // classStrings = ["Chinese Beijing Chinese Chinese Chinese Shanghai Chinese
-         // Macao", "Tokyo Japan Chinese"]
-         // classCounts = [3, 1]
-      }
+    public double classifyAll(String testDataFolder) throws IOException {
+        return 1.0;
+    }
 
-      for (int i = 0; i < numClasses; i++) {
-         String[] tokens = classStrings[i].split(" ");
-         classTokenCounts[i] = tokens.length; // T_ct
+    public static void main(String[] args) throws IOException {
+        // open test and train folders
+        // call classfier for training
 
-         // collecting the token counts
-         for (String token : tokens) {
-            vocabulary.add(token); // vocabulary is hashset, so no duplicate and you get the count unique
-                                   // terms/tokens
+        // call classfier for testing on test data
 
-            if (condProb[i].containsKey(token)) {
-               double count = condProb[i].get(token);
-               condProb[i].put(token, count + 1);
-            } else
-               condProb[i].put(token, 1.0);
-         }
-      }
+    }
 
-      // computing the class conditional probability using Laplace smoothing
-      for (int i = 0; i < numClasses; i++) {
-         Iterator<Map.Entry<String, Double>> iterator = condProb[i].entrySet().iterator();
-         int vSize = vocabulary.size();
-
-         while (iterator.hasNext()) {
-            Map.Entry<String, Double> entry = iterator.next(); // Beijing=1.0
-            String token = entry.getKey(); // Beijing
-            Double count = entry.getValue(); // 1.0
-            count = (count + 1) / (classTokenCounts[i] + vSize); // Laplace smoothing formula here and `count` is
-                                                                 // nothing but freq. of that token
-            condProb[i].put(token, count);
-         }
-
-         System.out.println("Class " + i + " class conditional probablities\n" + condProb[i]);
-      }
-   }
-
-   public int classify(String testDoc) {
-      int label = 0;
-      int vSize = vocabulary.size();
-      double[] score = new double[numClasses]; // class likelihood for each class
-
-      for (int i = 0; i < score.length; i++) {
-         // use log to avoid precision problems
-         score[i] = Math.log(classCounts[i] * 1.0 / trainingDocs.length); // prior probability of class --> why?
-         // 1. helps with imbalance in the class, 2. sets initial balance
-         System.out.println("score " + score[i]);
-      }
-
-      String[] tokens = testDoc.split(" ");
-
-      for (int i = 0; i < numClasses; i++) {
-
-         for (String token : tokens) {
-            // use log/addition to avoid precision problems
-            if (condProb[i].containsKey(token))
-               score[i] += Math.log(condProb[i].get(token)); // term's class conditional probability
-            else
-               score[i] += Math.log(1.0 / (classTokenCounts[i] + vSize)); // previously unknown term, compute its
-                                                                          // Laplace smoothed class conditional
-                                                                          // probability
-         }
-      }
-
-      double maxScore = score[0];
-
-      // find the largest class likelihood and save its label to return as the class
-      // value
-      for (int i = 0; i < score.length; i++) {
-         System.out.println("Class " + i + " likelihood = " + score[i]);
-         if (score[i] > maxScore) {
-            label = i;
-            maxScore = score[i];
-         }
-      }
-
-      return label;
-   }
-
-   public static void main(String[] args) {
-      String[] trainingDocs = { "Chinese Beijing Chinese",
-            "Chinese Chinese Shanghai",
-            "Chinese Macao",
-            "Tokyo Japan Chinese" };
-      int[] trainingLabels = { 0, 0, 0, 1 };
-      int numClass = 2;
-      NBClassifier nb = new NBClassifier(trainingDocs, trainingLabels, numClass);
-
-      String testDoc = "Chinese Chinese Chinese Tokyo Japan";
-      System.out.println("Class label = " + nb.classify(testDoc));
-   }
 }
---
+++ lab5/VSM.java
@@ -0,0 +1,167 @@
+package lab5;
+
+import java.util.*;
+
+public class VSM {
+   String[] myDocs;
+   ArrayList<String> termList;
+   ArrayList<ArrayList<Doc>> docLists;
+   double[] docLengthVec; // length vector for each document (used for cosine similarity computation)
+
+   public VSM(String[] docs) {
+      myDocs = docs;
+      termList = new ArrayList<String>();
+      docLists = new ArrayList<ArrayList<Doc>>();
+      ArrayList<Doc> docList;
+
+      // parse the documents to construct the vector space model
+      for (int i = 0; i < myDocs.length; i++) {
+         String[] tokens = myDocs[i].split(" ");
+         String token;
+
+         for (int j = 0; j < tokens.length; j++) {
+            token = tokens[j];
+
+            if (!termList.contains(token)) {
+               // new term
+               termList.add(token);
+               docList = new ArrayList<Doc>();
+               Doc doc = new Doc(i, 1); // initial raw frequency is 1
+               docList.add(doc); // add term to the postings list for this document
+               docLists.add(docList);
+            } else {
+               // already seen this term
+               int index = termList.indexOf(token);
+               docList = docLists.get(index);
+               boolean match = false; // is this a new document or not?
+               // search the postings for this document id, if match, update term frequency
+               for (Doc doc : docList) {
+                  if (doc.docId == i) { // we've seen this term in this document before
+                     doc.tw++;
+                     match = true;
+                     break;
+                  }
+               }
+               // if no match, we need to add another Doc object with a frequency of 1
+               if (!match) {
+                  Doc doc = new Doc(i, 1);
+                  docList.add(doc);
+               }
+            }
+         }
+      } // end of parsing
+
+      // compute the term tf-idf weights and the length vector for each document
+      int N = myDocs.length;
+      docLengthVec = new double[N];
+
+      for (int i = 0; i < termList.size(); i++) {
+         docList = docLists.get(i);
+         int df = docList.size(); // how many documents contain this term
+         Doc doc;
+
+         for (int j = 0; j < docList.size(); j++) {
+            doc = docList.get(j);
+            double tfidf = (1 + Math.log(doc.tw)) * Math.log(N / (df * 1.0));
+            docLengthVec[doc.docId] += Math.pow(tfidf, 2);
+            doc.tw = tfidf;
+            docList.set(j, doc);
+         }
+      }
+
+      // compute the magnitude of the length vector to use in cosine similarity
+      for (int i = 0; i < N; i++) {
+         docLengthVec[i] = Math.sqrt(docLengthVec[i]);
+      }
+   }
+
+   public void rankSearch(String[] query) {
+      // key is an integer that points to a document's score for this query
+      HashMap<Integer, Double> docs = new HashMap<Integer, Double>();
+      ArrayList<Doc> docList;
+
+      // find all documents with any matching query keywords
+      for (String term : query) {
+         int index = termList.indexOf(term);
+
+         if (index < 0)
+            continue; // ignore this keyword
+
+         docList = docLists.get(index);
+         double w_t = Math.log((myDocs.length * 1.0) / docList.size()); // N / df of keyword
+         Doc doc;
+
+         for (int j = 0; j < docList.size(); j++) {
+            doc = docList.get(j);
+            double score = w_t * doc.tw; // (need to normalize to perform cosine similarity)
+
+            if (!docs.containsKey(doc.docId)) { // is this doc in hash map yet?
+               docs.put(doc.docId, score); // add to hash map
+            } else {
+               score += docs.get(doc.docId); // add to previous score due to multiple keyword matches
+               docs.put(doc.docId, score);
+            }
+         }
+      }
+      System.out.println(docs); // output document scores
+   }
+
+   public String toString() {
+      String matrixString = new String();
+      ArrayList<Doc> docList;
+
+      for (int i = 0; i < termList.size(); i++) {
+         matrixString += String.format("%-15s", termList.get(i));
+         docList = docLists.get(i);
+
+         for (int j = 0; j < docList.size(); j++)
+            matrixString += docList.get(j) + "\t";
+
+         matrixString += "\n";
+      }
+
+      return matrixString;
+   }
+
+   public static void main(String[] args) {
+      String[] docs = { "new home sales top forecasts", // top and forecasts are rare terms
+            "home sales rise in july",
+            "increase in home sales in july", // in has a higher weight since it appears twice
+            "july new home sales rise" };
+
+      VSM vsm = new VSM(docs);
+      System.out.println(vsm);
+
+      // test cases
+      // 1: in
+      // 2: in, top
+      // 3: in, july
+      // 4: nothing, no, new, home
+      // String[] query = {"in"};
+      // String[] query = {"in", "top"};
+      // String[] query = {"in", "july"};
+      String[] query1 = { "in" };
+      String[] query2 = { "in", "top" };
+      String[] query3 = { "in", "july" };
+      String[] query4 = { "nothing", "no", "new", "home" };
+      vsm.rankSearch(query1);
+      vsm.rankSearch(query2);
+      vsm.rankSearch(query3);
+      vsm.rankSearch(query4);
+   }
+}
+
+class Doc {
+   int docId;
+   double tw; // term's weight in this document
+
+   public Doc(int did, double weight) {
+      docId = did;
+      tw = weight;
+   }
+
+   public String toString() {
+      String docIdString = docId + ": " + tw;
+      return docIdString;
+   }
+}
